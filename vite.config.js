96ab95b2a77400a0675b41868ff4585690882611
JhulerCode <jhuler.code@gmail.com>
2026-06-30 18:07:41 -0500

    Add manifest.json for Progressive Web App support
    - Introduced a new manifest file to enhance the web app's capabilities.
    - Included app metadata such as name, description, and theme colors.
    - Added icons and screenshots for better user experience on installation.

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueDevTools(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            // registerType: 'prompt',
-            workbox: {
-                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
-            },
-            includeAssets: ['robots.txt', 'apple-touch-icon.png'], //apple 180x180 px
-            manifest: {
-                name: 'DivergeRest',
-                short_name: 'DivergeRest',
-                description: 'DivergeRest',
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#ffffff',
-                theme_color: '#2492c2',
-                icons: [
-                    {
-                        src: 'pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-maskable-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable'
-                    }
-                ],
-                screenshots: [
-                    {
-                        src: 'screenshot1.png',
-                        // sizes: '540x720',
-                        type: 'image/png',
-                        form_factor: 'wide'
-                    },
-                    {
-                        src: 'screenshot2.png',
-                        // sizes: '540x720',
-                        type: 'image/png'
-                        // sin form_factor para mobile
-                    }
-                ]
-            }
-        })
-    ],
+    plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 })
